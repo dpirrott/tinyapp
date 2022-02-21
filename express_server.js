3293cb559037a63c98ee94dc28be0559a9e8e52a
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-02-21 14:55:44 -0800

    Created random string generator function to help with handling new url entries via post request

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -9,6 +12,15 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  let randomString = "";
+  const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for (let i = 0; i < 6; i++) {
+    randomString += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return randomString;
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -16,6 +28,17 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase }
   res.render('urls_index', templateVars);
+});
+
+app.post("/urls", (req,res) => {
+  const longURL = req.body.longURL;
+  const shortURL = generateRandomString();
+  console.log(`Short URL: ${shortURL}, Long URL: ${longURL}`);
+  res.send(`Short URL: ${shortURL}, Long URL: ${longURL}`);
+});
+
+app.get("/urls/new", (req,res) => {
+  res.render('urls_new');
 });
 
 app.get("/urls/:shortURL", (req, res) => {
