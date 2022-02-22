7dcf47eba5a07d8d9ef9b30a0af8040032236eb6
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-02-22 13:45:39 -0800

    Added form html code for login, next create route in server

--- express_server.js
+++ express_server.js
@@ -25,6 +25,10 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.post("/login", (req,res) => {
+  
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase }
   res.render('urls_index', templateVars);
